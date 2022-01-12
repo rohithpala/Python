403f9f85491f978bfd504292b32e80b7fab80489
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-12 20:44:39 +0530

    lambda, filter, map and reduce

--- functionalProgramming.py
+++ functionalProgramming.py
@@ -7,12 +7,10 @@ print(a(2, 4))
 # filter (filters values of iterable based on return value of boolean function)
 lst = [1, 2, 3, 4, 5, 6]
 evens = filter(lambda x: x % 2 == 0, lst)
-print(evens)
 print(list(evens))
 
 # map (maps the values of iterable to the return value of the function)
 mapped = map(lambda x: x ** 2, lst)
-print(mapped)
 print(list(mapped))
 
 # reduce (reducing iterable into signle value) (a method from the 'reduce' module)
