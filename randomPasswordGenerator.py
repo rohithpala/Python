1a69b9a7b889d9b35d1fb657776cf0156ede7859
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-12 19:44:14 +0530

    Generating Random Password in Python

---
+++ randomPasswordGenerator.py
@@ -0,0 +1,12 @@
+from random import sample
+import string
+
+
+def passwordGenerator(passwordLength=8):
+    allChars = string.ascii_letters + string.digits + string.punctuation
+    return "".join(sample(allChars, passwordLength))
+
+
+if __name__ == "__main__":
+    passwordLength_ = int(input("What is the Password Length: "))
+    print("Your Password is", passwordGenerator(passwordLength_))
