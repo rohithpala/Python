134faf72810b9a7b3f9773579004c1246dfb9f3e
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-12 20:36:40 +0530

    Prime Factorizing a Number

--- primeFactorization.py
+++ primeFactorization.py
@@ -8,19 +8,33 @@ def isPrime(n):
     return True
 
 
-def primeFactorization(num):
-    primeFactorizationResult = {}
+def primeFactorization(num, primes):
+    primeFactors = {}
     for prime in primes:
-        if num % prime == 0:
-            primeFactorizationResult[prime] = int(floor(log(num, prime)))
-            num //= pow(prime, primeFactorizationResult[prime])  # Problem Here
-    return primeFactorizationResult
+        while True:
+            if num % prime == 0:
+                primeFactors[prime] = primeFactors.get(prime, 0) + 1
+                num //= prime
+            else:
+                break
+    return primeFactors
 
 
 if __name__ == "__main__":
     n = int(input("Enter a number: "))
-    primes = []
-    for i in range(2, n // 2 + 1):
-        if isPrime(i):
-            primes.append(i)
-    print(primeFactorization(n))
+    if isPrime(n):
+        print(n, "= 1 *", n)
+    else:
+        primes = []
+        for i in range(2, n // 2 + 1):
+            if isPrime(i):
+                primes.append(i)
+        primeFactors = primeFactorization(n, primes)
+        noOfPrimes = len(primeFactors)
+        print(n, "=", end=" ")
+        for base, power in primeFactors.items():
+            print(str(base) + "^" + str(power), end="")
+            if noOfPrimes != 1:
+                print(" * ", end="")
+            noOfPrimes -= 1
+        print("\nBase:Power Form: ", primeFactors, end="")
