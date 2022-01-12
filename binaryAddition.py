7f622713c4c35718931ad379b1b51e7c9e2454a6
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-12 20:41:27 +0530

    Binary Addition

--- binaryAddition.py
+++ binaryAddition.py
@@ -1,28 +1,30 @@
 bin1 = input("Enter Binary Number 1: ")
 bin2 = input("Enter Binary Number 2: ")
 
-len1 = len(bin1)
-len2 = len(bin2)
+print(bin(int(bin1, 2) + int(bin2, 2))[2:])
 
-if len1 > len2:
-    bin2 = bin2.rjust(len1, "0")
-else:
-    bin1 = bin1.rjust(len2, "0")
+# len1 = len(bin1)
+# len2 = len(bin2)
 
-s = ""
-carry = 0
-for i in range(len1 - 1, -1, -1):
-    sod = int(bin1[i]) + int(bin2[i])  # sum of digits
-    carry = 0
-    if sod > 1:
-        carry = 1
-        s += "0"
-    else:
-        s += str(sod)
+# if len1 > len2:
+#     bin2 = bin2.rjust(len1, "0")
+# else:
+#     bin1 = bin1.rjust(len2, "0")
 
-s = s[::-1]
+# s = ""
+# carry = 0
+# for i in range(len1 - 1, -1, -1):
+#     sod = int(bin1[i]) + int(bin2[i])  # sum of digits
+#     carry = 0
+#     if sod > 1:
+#         carry = 1
+#         s += "0"
+#     else:
+#         s += str(sod)
 
-if carry == 1:
-    s = "1" + s
+# s = s[::-1]
 
-print("Sum:", s)
+# if carry == 1:
+#     s = "1" + s
+
+# print("Sum:", s)
